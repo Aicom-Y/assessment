6d78038747cf206c69166403d0e0887aa0b396b5
Aicom-Y <44220990+Aicom-Y@users.noreply.github.com>
2018-10-17 13:54:10 +0900

    Create assessment.js

---
+++ assessment.js
@@ -0,0 +1,107 @@
+(function () {
+    'use strict';
+    const userNameInput = document.getElementById('user-name');
+    const assessmentButton = document.getElementById('assessment');
+    const resultDivided = document.getElementById('result-area');
+    const tweetDivided = document.getElementById('tweet-area');
+
+    /**
+    * 指定した要素の子どもを全て削除する
+    * @param {HTMLElement} element HTMLの要素
+    */
+    function removeAllChildren(element) {
+        while (element.firstChild) {//resultdivided（＝'result-area)タグの下に最初の子供要素がある限り(ほんとは子要素を返してるんだろうけど、なんかあればtrueとも受け取れる？)
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    userNameInput.onkeydown = (event) => {
+        if (event.key == 'Enter') {
+            assessmentButton.onclick();//assessmentButton.onclick(); では、プロパティ onclick に代入されている関数を呼び出す記述
+        }
+    };
+
+    assessmentButton.onclick = () => {
+        const userName = userNameInput.value;
+        if (userName.length === 0) { // 名前が空の時は処理を終了する ガード句
+            return;// return; は、戻り値なしに、そこで処理を終了するという意味
+        }
+        //アロー関数 無名関数といわれる名前を持たない関数の記述法で、それを assessmentButton というオブジェクトの onclick という プロパティに設定することで、ボタンがクリックされた時に動くようにできます
+
+        //診断結果表示エリアの作成
+        removeAllChildren(resultDivided);
+        const header = document.createElement('h3');
+        header.innerText = '診断結果';
+        resultDivided.appendChild(header);//resultDivided（=id result-area）の一番下にheader(=h3)を入れる）
+
+        const paragraph = document.createElement('p');
+        const result = assessment(userName);
+        paragraph.innerText = result;
+        resultDivided.appendChild(paragraph);
+
+
+        removeAllChildren(tweetDivided);
+        const anchor = document.createElement('a');
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' + encodeURIComponent('あなたのいいところ') + '&ref_src=twsrc%5Etfw';
+        anchor.setAttribute('href', hrefValue);
+        anchor.className = 'twitter-hashtag-button';
+        anchor.setAttribute('data-text', result);
+        anchor.innerText = 'Tweet #あなたのいいところ';
+        tweetDivided.appendChild(anchor);
+
+        twttr.widgets.load();
+    };
+
+    const answers = [
+        '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+        '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+        '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+        '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+        '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+        '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+        '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+        '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+        '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+        '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+        '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+        '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+        '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+        '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+        '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+        '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+
+    ];
+
+    /**
+    * 名前の文字列を渡すと診断結果を返す関数
+    * @param {string} userName ユーザーの名前
+    * @return {string} 診断結果
+    */
+    function assessment(userName) {
+        //全文字のコード番号を取得して、それを足し合わせる
+        let sumOfcharCode = 0;
+        for (let i = 0; i < userName.length; i++) {
+            sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+        }
+        //文字のコード番号の合計を回答の数で割って添え字の数値を求める
+        const index = sumOfcharCode % answers.length;
+        let result = answers[index];
+
+        result = result.replace(/\{userName\}/g, userName);
+        ///\{userName\}/g と書かれている部分が正規表現です。{userName} という文字列自身に合うものを複数回適用するという意味です。
+
+        return result;
+    }
+    // テストコード
+    console.assert(
+        assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+        '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+    );
+    console.assert(
+        assessment('太郎') === assessment('太郎'),
+        '入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
+    );
+
+
+
+})();
